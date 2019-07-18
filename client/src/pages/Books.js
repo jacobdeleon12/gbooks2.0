847dba73a33dcdd5345daa9aeb23c44e04b0e448
Jacob DeLeon <jacobdeleon12@gmail.com>
2019-07-17 17:29:26 -0700

    trying to figure it out

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -5,8 +5,8 @@ import API from "../utils/API";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
-import { Input,  FormBtn } from "../components/Form";
-import ResultsSeach from "../components/Results-search"
+import { Input, FormBtn } from "../components/Form";
+import ResultsSeach from "../components/Results-search";
 // import { Query } from "mongoose";
 
 class Books extends Component {
@@ -15,9 +15,9 @@ class Books extends Component {
     title: "",
     author: "",
     synopsis: "",
-    search:"",
-    result:{},
-    bookData:{}
+    search: "",
+    result: {},
+    bookData: {}
   };
 
   // componentDidMount() {
@@ -45,63 +45,63 @@ class Books extends Component {
     });
   };
 
-  seachGbooks = bookTitle =>{
+  seachGbooks = bookTitle => {
     API.searchBooks(bookTitle)
-    .then(res=>{
-      this.setState({ result: res.data })
-      // console.log(res.data)
-      // console.log(this.state.result.items);
-      this.state.result.items.forEach(items => {
-        // console.log(items.volumeInfo);
+      .then(res => {
+        this.setState({ result: res.data });
+        // console.log(res.data);
+        // console.log(this.state.result.items);
+        this.state.result.items.forEach(items => {
+          // console.log(items.volumeInfo);
 
-        //for titles of book
-        let title = items.volumeInfo.title
-        // console.log(items.volumeInfo.title);
-        // for authers
-        var info =items.volumeInfo.authors
-        let authers = []
-        for (let i = 0; i < info.length; i++) {
-          // console.log(info[i]);
-          authers.push(info[i])
-        }
-        let authersinfo= authers.toString().split(",").join(" and ");
-        // description
-        // console.log(items.volumeInfo.description);
-        let description = items.volumeInfo.description
-        // thumbnail
-        // console.log(items.volumeInfo.imageLinks.thumbnail);
-        let img = items.volumeInfo.imageLinks.thumbnail
-        // link to previewLink
-        // console.log(items.volumeInfo.previewLink);
-        let previewLink = items.volumeInfo.previewLink
+          //for titles of book
+          let title = items.volumeInfo.title;
+          // console.log(items.volumeInfo.title);
+          // for authers
+          var info = items.volumeInfo.authors;
+          let authers = [];
+          for (let i = 0; i < info.length; i++) {
+            // console.log(info[i]);
+            authers.push(info[i]);
+          }
+          let authersinfo = authers
+            .toString()
+            .split(",")
+            .join(" and ");
+          // description
+          // console.log(items.volumeInfo.description);
+          let description = items.volumeInfo.description;
+          // thumbnail
+          // console.log(items.volumeInfo.imageLinks.thumbnail);
+          let img = items.volumeInfo.imageLinks.thumbnail;
+          // link to previewLink
+          // console.log(items.volumeInfo.previewLink);
+          let previewLink = items.volumeInfo.previewLink;
 
-        this.setState({bookData:{
-          title:title,
-          authors:authersinfo,
-          description:description,
-          img:img,
-          previewLink:previewLink
-        }})
+          this.setState({
+            bookData: [{
+              title: title,
+              authors: authersinfo,
+              description: description,
+              img: img,
+              previewLink: previewLink
+            }]
+          });
 
-        // console.log(this.state.bookData);
-        // console.log(this.state.bookData.authors);
-        
-        
-      });
-
-      
-    }).catch(err => console.log(err));
-  }
+          console.log(this.state.bookData);
+          // console.log(this.state.bookData.authors);
+        });
+      })
+      .catch(err => console.log(err));
+  };
 
   handleFormSubmit = event => {
     // console.log(this.state);
-    
+
     event.preventDefault();
     // this.setState()
-    this.seachGbooks(this.state.title)
-    
+    this.seachGbooks(this.state.title);
   };
-
 
   render() {
     return (
@@ -117,10 +117,9 @@ class Books extends Component {
                 onChange={this.handleInputChange}
                 name="title"
                 placeholder="Title (required)"
-
               />
               <FormBtn
-                disabled={!(this.state.title)}
+                disabled={!this.state.title}
                 onClick={this.handleFormSubmit}
               >
                 Find Book
@@ -131,8 +130,8 @@ class Books extends Component {
             <Jumbotron>
               <h1>Books On My List</h1>
             </Jumbotron>
-           
-        {/* {console.log(this.state.bookData)} */}
+
+            {/* {console.log(this.state.bookData)} */}
             {/* {console.log(this.state.bookData)}
             {this.state.bookData.length ? (
               <List>
@@ -152,19 +151,25 @@ class Books extends Component {
               <h3>No Results to Display</h3>
             )} */}
 
-            
+            {/* {this.state.bookData.map(book =>{
             <ResultsSeach 
+            thumbnail = {book.img}
+            title = {book.title}
+            auther = {book.authors}
+            description = {book.description}
+            // link = {this.state.bookData.previewLink}
+            >
+            </ResultsSeach>
+            })} */}
+
+            {/* <ResultsSeach 
             thumbnail = {this.state.bookData.img}
             title = {this.state.bookData.title}
             auther = {this.state.bookData.authors}
             description = {this.state.bookData.description}
             // link = {this.state.bookData.previewLink}
             >
-      
-            </ResultsSeach>
-          
-          
-
+            </ResultsSeach> */}
           </Col>
         </Row>
       </Container>
